c025d37fed68444ee58931e96f2f267daab6d455
Metaluim <rikajff@gmail.com>
2015-01-22 16:22:23 +0000

    Fixed compilation issue.

--- kmain.c
+++ kmain.c
@@ -13,11 +13,12 @@ void
 kmain(void)
 {
 	int i;
+	unsigned char s[] = " ";
 
 	init();
 
 	for (i = 0; i < FB_NUM_CELLS; ++i)
 	{
-		dev_write(2, (unsigned char *) " ", 1);
+		dev_write(1, s, 1);
 	}
 }
