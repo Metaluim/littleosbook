17af4196d809ee48610d828002472a360a7681a3
Metaluim <rikajff@gmail.com>
2015-01-26 22:10:06 +0000

    Added segment descriptor entry.

--- gdt.h
+++ gdt.h
@@ -7,10 +7,22 @@ struct gdt
 	unsigned short	size;
 } __attribute__((packed));
 
+struct gdt_entry
+{
+	unsigned short	limit;
+	unsigned short	base1;
+	unsigned char	base2;
+	unsigned char	dpl;
+	unsigned char	limit2;
+	unsigned char	base3;
+} __attribute__((packed));
+
 void
 init_gdt(void);
 
 extern void
 load_gdt(struct gdt g);
+
+
 
 #endif /* _GDT_H */
